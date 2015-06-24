8f65f7bb31344e9464be75c99123185b2723cbd8
pascalweiss <pascal@mbp111.local>
2015-06-24 15:02:20 +0200

    initial commit

--- config.php
+++ config.php
@@ -4,15 +4,15 @@
 date_default_timezone_set('Europe/Berlin');
 
 //site address
-define('DIR','http://localhost:8888/HTWRoomFinder/');
+define('DIR','http://localhost:8888/HTWRoomFinder/'); // e.g. define('DIR','http://localhost:8888/HTWRoomFinder/');
 define('DOCROOT', dirname(__FILE__));
 
 // Credentials for the HTW server
 define('DB_TYPE','pgsql');
-define('DB_HOST','db.f4.htw-berlin.de');
-define('DB_NAME','_s0544768__htwroomfinder');
-define('DB_USER','_s0544768__htwroomfinder_generic');
-define('DB_PASS','13h%mdI-Sa#DevPSQ');
+define('DB_HOST','');
+define('DB_NAME','');
+define('DB_USER','');
+define('DB_PASS','');
 
 //set prefix for sessions
 define('SESSION_PREFIX','HTWRoomFinder');
